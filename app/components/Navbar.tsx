af7e6addabbff49ddf8524e9f20b1616906e12a0
weeezik <mkihanya.33@gmail.com>
2025-05-03 15:21:40 -0700

    slight changes to navbar color and background color

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import UserMenu from './UserMenu';
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between bg-white rounded-xl mt-6 mx-8 px-8 py-4 shadow-sm">
+    <nav className="flex items-center justify-between bg-white rounded-xl mt-6 mx-8 mb-6 px-8 py-4 shadow-sm">
       <Link href="/landing" className="text-2xl font-medium text-gray-900 hover:underline">Resumeganizer</Link>
       <div className="flex gap-8 text-lg font-normal text-gray-900 items-center">
         <Link href="/resumes" className="hover:underline">Resumes</Link>
